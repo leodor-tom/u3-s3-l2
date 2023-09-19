afe6841acf498c833a4799f8faedcb124d35dcee
leodor-tom <eleodoritom@gmail.com>
2023-09-19 15:57:18 +0200

    first commit

---
+++ src/redux/store/index.js
@@ -0,0 +1,30 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import favoritesReducer from "../reducers/favoritesReducer";
+import jobsReducer from "../reducers/jobsReducer";
+import { encryptTransform } from "redux-persist-transform-encrypt";
+import persistReducer from "redux-persist/es/persistReducer";
+import persistStore from "redux-persist/es/persistStore";
+// import mainReducer from '../reducers'
+
+const persistConfig = {
+  key: "root",
+  storage,
+  transforms: [
+    encryptTransform({
+      secretKey: process.env.REACT_APP_PERSIST_KEY,
+    }),
+  ],
+};
+const rootReducer = combineReducers({
+  favorites: favoritesReducer,
+  jobs: jobsReducer,
+});
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware => getDefaultMiddleware({ serializableCheck: false }),
+});
+
+export const persistor = persistStore(store);
